e980d2da39d492ff4b7d72daa4ca907edae6e5d9
Anirudh Balasubramaniam <anirudhnkl@gmail.com>
2018-03-12 01:33:47 -0700

    Initial Commit

---
+++ Project_4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project_4
+//
+//  Created by Anirudh Balasubramaniam on 3/12/18.
+//  Copyright © 2018 Anirudh Balasubramaniam. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
